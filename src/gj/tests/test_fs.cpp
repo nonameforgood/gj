c0b111043b75662280a20ea3251b8062772b2ebb
RSM <rsmRSM@Michels-iMac.local>
2023-04-10 12:32:30 -0400

    fix FS test on esp32

--- src/gj/tests/test_fs.cpp
+++ src/gj/tests/test_fs.cpp
@@ -11,13 +11,18 @@ static void TestFSTerminalHandler(const char *string)
 
 static void WaitTestFSOutputCount(uint32_t count, uint32_t timeout)
 {
-  const uint32_t begin = GetElapsedMillis();
+  uint32_t begin = GetElapsedMillis();
+
+  uint32_t currentCount = g_testFSSerialHandlerCount;
 
   while(g_testFSSerialHandlerCount != count)
   {
     GJEventManager->WaitForEvents(0);
 
     Delay(5);
+
+    if (currentCount != g_testFSSerialHandlerCount)
+      begin = GetElapsedMillis();
 
     const uint32_t end = GetElapsedMillis();
 
@@ -33,17 +38,17 @@ void TestFS()
   g_testFSSerialHandlerCount = 0;
   InterpretCommand("fs info");
   WaitTestFSOutputCount(3, 50);
-  TEST_CASE_VALUE_INT32("fs info, 3 outputs", g_testFSSerialHandlerCount, 3, 3);
+  TEST_CASE_VALUE_INT32("fs info, 3 outputs", g_testFSSerialHandlerCount, 3, 12);
 
   g_testFSSerialHandlerCount = 0;
   InterpretCommand("fs dump /test");
-  WaitTestFSOutputCount(10, 500);
-  TEST_CASE_VALUE_INT32("fs dump /test, 3 outputs", g_testFSSerialHandlerCount, 3, 10);
+  WaitTestFSOutputCount(10, 50);
+  TEST_CASE_VALUE_INT32("fs dump /test, 3 outputs", g_testFSSerialHandlerCount, 1, 10);
 
   g_testFSSerialHandlerCount = 0;
   InterpretCommand("fs dumphex /test");
-  WaitTestFSOutputCount(34, 500);
-  TEST_CASE_VALUE_INT32("fs dumphex /test, 3 outputs", g_testFSSerialHandlerCount, 34, 34);
+  WaitTestFSOutputCount(34, 50);
+  TEST_CASE_VALUE_INT32("fs dumphex /test, 3 outputs", g_testFSSerialHandlerCount, 1, 34);
 
   RemoveTerminalHandler(terminalHandle);
 }    
